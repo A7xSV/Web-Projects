e961643527d2b77f5f9af38a14783955386126f0
A7xSV <sabhya.verma16@gmail.com>
2018-05-15 17:17:54 +0530

    Add important flag

--- Angular/directives-test/src/app/better-highlight/better-highlight.directive.ts
+++ Angular/directives-test/src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2, OnInit } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, RendererStyleFlags2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -8,6 +8,6 @@ export class BetterHighlightDirective implements OnInit {
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'lightblue');
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'lightblue', RendererStyleFlags2.DashCase | RendererStyleFlags2.Important);
   }
 }
