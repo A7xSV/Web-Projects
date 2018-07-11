13575fdce2c4abf6e30fd1d800162ec8010c028e
A7xSV <sabhya.verma16@gmail.com>
2018-07-12 02:32:21 +0530

    Pass queryParams

--- Angular/routing-start/src/app/servers/server/server.component.ts
+++ Angular/routing-start/src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -11,7 +11,7 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id']; // Since the URL params are parsed as a string, the plus(+) sign can be used to convert it to a number
@@ -25,4 +25,8 @@ export class ServerComponent implements OnInit {
     );
   }
 
+  onEdit() {
+    this.router.navigate(['edit'], { relativeTo: this.route, queryParamsHandling: 'preserve' });
+    // Use 'merge' for queryParamsHandling, if adding more params while navigating
+  }
 }
