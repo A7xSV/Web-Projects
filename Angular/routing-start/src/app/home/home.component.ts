575350b6de66ce33fc0cb7a7746848c41a526d93
A7xSV <sabhya.verma16@gmail.com>
2018-07-25 16:24:27 +0530

    Use AuthService

--- Angular/routing-start/src/app/home/home.component.ts
+++ Angular/routing-start/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -18,5 +19,13 @@ export class HomeComponent implements OnInit {
     console.log(this.route);
     // By default, unlike the routerLink, the navigate method does not know the route on which it sits. Thus navigates relative to the root by deafult unless explicitly mentioned
     this.router.navigate(['/servers', id, 'edit'], {relativeTo: this.route, queryParams: {allowEdit: '1'}, fragment: 'loaded'});
+  }
+
+  onLogin() {
+    this.authService.logIn();
+  }
+
+  onLogout() {
+    this.authService.logOut();
   }
 }
